d2fd39c1a118c867fb3a45bf377e59081a3266f8
KhushiKV <78869960+KhushiKV@users.noreply.github.com>
2021-10-13 15:44:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, session, render_template, request,make_response,redirec
 # from flask_mysql import MySQL
 from flaskext.mysql import MySQL
 
-# instatiating flask class 
+# instatiating flask class
 app=Flask(__name__)
 mysql = MySQL()
 
 # configuring MySQL for the web application
 app.config['MYSQL_DATABASE_USER'] = 'root'    # default user of MySQL to be replaced with appropriate username
-app.config['MYSQL_DATABASE_PASSWORD'] = '' # default passwrod of MySQL to be replaced with appropriate password
-app.config['MYSQL_DATABASE_DB'] = 'pathology'  # Database name to be replaced with appropriate database name
+app.config['MYSQL_DATABASE_PASSWORD'] = 'KHU1*ver' # default passwrod of MySQL to be replaced with appropriate password
+app.config['MYSQL_DATABASE_DB'] = 'customersdb'  # Database name to be replaced with appropriate database name
 app.config['MYSQL_DATABASE_HOST'] = 'localhost' # default database host of MySQL to be replaced with appropriate database host
 #initialise mySQL
 mysql.init_app(app)
@@ -21,3 +21,9 @@ conn = mysql.connect()
 def home():
     # return("HELLO!!")
     return render_template("home.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
